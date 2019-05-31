3f08cff8e01f9b58ff7507692dd7a161d0b08950
王鑫玉 <apathyjade@outlook.com>
2019-05-31 19:12:44 +0800

    feat:配置文件增加promise增加，优化代码逻辑啊

--- index.js
+++ index.js
@@ -11,20 +11,32 @@ const url = require('url')
 const qs = require('querystring')
 
 const cwd = process.cwd()
-const noop = function() {}
-// 配置
-const config = Object.assign({
-  path: path.resolve(cwd, 'mock'),
-  callback: ['callback', 'jsonpCallback'],
-  hooks: {
 
+// 初始化配置
+let initCfg
+
+// 获取配置文件 没有则先初始化
+const getCfg = (() => {
+  let config = null
+  initCfg = () => {
+    config = Object.assign({
+      path: path.resolve(cwd, 'mock'),
+      callback: ['callback', 'jsonpCallback'],
+      hooks: {}
+    }, require(path.resolve(cwd, '.mock.config')))
+    
+    // 处理 callback 为 Array 类型
+    if (typeof config.callback === 'string') {
+      config.callback = [config.callback]
+    }
   }
-}, require(path.resolve(cwd, '.mock.config')))
-
-// 处理 callback 为 Array 类型
-if (typeof config.callback === 'string') {
-  config.callback = [config.callback]
-}
+  return () => {
+    if (!config) {
+      initCfg()
+    }
+    return config
+  }
+})()
 
 // 延迟执行
 const delay = (fn, t = 400) => {
@@ -35,6 +47,7 @@ const delay = (fn, t = 400) => {
 
 // 通过path 获取 mock数据
 const getFileByPath = uriPath => {
+  const config = getCfg()
   const mockPath = path.resolve(config.path, config.map[uriPath] || `.${uriPath}`)
   try {
     let backData = require(mockPath)
@@ -52,31 +65,50 @@ const getFileByPath = uriPath => {
   }
 }
 
-module.exports =  function(req, res) {
+// 处理 mock 文件内容 转成 string 类型
+const dealMock2Str = (data) => {
+
+  // 字符串直接返回
+  if (typeof data === 'string') {
+    return data
+  }
+
+  // 对于对象 转成 字符串
+  if (typeof data === 'object') {
+    return JSON.stringify(data)
+  }
+
+  // 对于其他非字符串类型 转成字符串
+  return String(data)
+}
+
+function mock (req, res) {
 
   // 格式化 url 和 url参数
   const urlObj = url.parse(req.url)
   const params = qs.parse(urlObj.query)
+  const config = getCfg()
   // 读取本地 mock 文件内容
   let data = getFileByPath(urlObj.pathname)
+
   // mock 配置是动态方法时 先执行脚本
   if (typeof data === 'function') {
     data = data(req, res)
   }
-  // 对于对象 转成 字符串
-  if (typeof data === 'object') {
-    data = JSON.stringify(data)
-  }
 
-  // 对于其他非字符串类型 转成字符串
-  if (typeof data !== 'string') {
-    data = String(data)
+  data instanceof Promise ? data.then(dealData) : dealData(data)
+
+  function dealData (data) {
+    data = dealMock2Str(data)
+    
+    // 由于 mock 数据同步获取到 返回过快的话 前端的异步效果会同步既视感  这里做一个，异步延迟返回
+    delay(() => {
+      // 处理 jsonp 返回 配置
+      let cbKey = config.callback.find(it => params[it])
+      let callback = cbKey && params[cbKey]
+      res.end(callback ? `${callback}(${data})` : data)
+    })
   }
-  // 由于 mock 数据同步获取到 返回过快的话 前端的异步效果会同步既视感  这里做一个，异步延迟返回
-  delay(() => {
-    // 处理 jsonp 返回 配置
-    let cbKey = config.callback.find(it => params[it])
-    let callback = cbKey && params[cbKey]
-    res.end(callback ? `${callback}(${data})` : data)
-  })
 }
+mock.initCfg = initCfg
+module.exports = mock
--- test/mock/a.3.js
+++ test/mock/a.3.js
@@ -6,4 +6,10 @@
  * @Last Modified Time: 2019-05-30 19:03:50
  */
 
-module.exports = 'asd1123'
+module.exports = async function () {
+  return new Promise((r) => {
+    setTimeout(() => {
+      r('1qqqqwqw')
+    }, 200)
+  })
+}
